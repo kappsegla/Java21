def2c51acee99e6ca1737ad12edc99e201ac6047
Martin Blomberg <martin.blomberg@outlook.com>
2021-08-27 08:40:00 +0200

    Adds Exercise solution package

---
+++ src/se/iths/java21/exercises/Chapter1.java
@@ -0,0 +1,14 @@
+package se.iths.java21.exercises;
+
+public class Chapter1 {
+
+    public static void main(String[] args) {
+        task2();
+    }
+
+    public static void task2(){
+
+    }
+
+}
+
