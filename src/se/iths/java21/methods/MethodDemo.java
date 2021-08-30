730ab836e9e5fcd4d3647e8a3fa98d6cad88928a
Martin <martin.blomberg@outlook.com>
2021-08-30 15:25:56 +0200

    Adds method demo

--- src/se/iths/java21/methods/MethodDemo.java
+++ src/se/iths/java21/methods/MethodDemo.java
@@ -3,13 +3,26 @@ package se.iths.java21.methods;
 public class MethodDemo {
     public static void main(String[] args) {
         printMultiplicationTable2();
-        printMultiplicationTable2();
-        printMultiplicationTable2();
+        int value = 2;
+        printMultiplicationTable(value);
+        printMultiplicationTable(3);
+
+        int x = add(5,6);
+        int x2 = 1 + add(2,2) * add(1,1);
+
     }
 
-    public static void printMultiplicationTable2() {
-        for (int i = 1; i <= 10; i++)
-            System.out.println(2 * i);
+    private static void printMultiplicationTable2() {
+        for (int i2 = 1; i2 <= 10; i2++)
+            System.out.println(2 * i2);
     }
 
+    private static void printMultiplicationTable(final int table) {
+        for (int i2 = 1; i2 <= 10; i2++)
+            System.out.println(table * i2);
+    }
+
+    private static int add(int tal1, int tal2){
+        return tal1 + tal2;
+    }
 }
