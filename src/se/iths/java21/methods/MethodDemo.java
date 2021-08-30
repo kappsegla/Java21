95949ac0b2caa311059acdb94dc8945a3fba003d
Martin <martin.blomberg@outlook.com>
2021-08-30 14:06:40 +0200

    Adds method demo

---
+++ src/se/iths/java21/methods/MethodDemo.java
@@ -0,0 +1,15 @@
+package se.iths.java21.methods;
+
+public class MethodDemo {
+    public static void main(String[] args) {
+        printMultiplicationTable2();
+        printMultiplicationTable2();
+        printMultiplicationTable2();
+    }
+
+    public static void printMultiplicationTable2() {
+        for (int i = 1; i <= 10; i++)
+            System.out.println(2 * i);
+    }
+
+}
