86e5b3afa323b351a809d82f7043db07b5c9f7b7
Martin <martin.blomberg@outlook.com>
2021-08-31 13:36:02 +0200

    Added growth of array length

--- src/se/iths/java21/arrays/DynamicArray.java
+++ src/se/iths/java21/arrays/DynamicArray.java
@@ -1,17 +1,30 @@
 package se.iths.java21.arrays;
 
+import java.util.ArrayList;
+
 public class DynamicArray {
 
     int[] values = new int[10];
     private int count = 0;
 
-    void add(int value){
+    void add(int value) {
+        if (count == values.length)
+            grow();
         values[count] = value;
         count = count + 1;
     }
 
-    int removeLast(){
-        if( count == 0)
+    private void grow() {
+        //Create a new array with double size
+        int[] newArray = new int[values.length * 2];
+        //Copy data from old array to new array
+        System.arraycopy(values, 0, newArray, 0, values.length);
+        //Set values reference to point to new array in memory
+        values = newArray;
+    }
+
+    int removeLast() {
+        if (count == 0)
             return 0;
         return values[--count];
     }
@@ -25,7 +38,7 @@ public class DynamicArray {
         DynamicArray dynamicArray = new DynamicArray();
         dynamicArray.add(1);
         dynamicArray.add(2);
-        for (int value: dynamicArray.values)
+        for (int value : dynamicArray.values)
             System.out.println(value);
     }
 }
