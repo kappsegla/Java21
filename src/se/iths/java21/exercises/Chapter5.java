2c72f4f38200b85c1790c620bfb3b78ea27da9bb
Martin Blomberg <martin.blomberg@outlook.com>
2021-09-03 11:48:25 +0200

    Adds exercise Chapter5 task3

--- src/se/iths/java21/exercises/Chapter5.java
+++ src/se/iths/java21/exercises/Chapter5.java
@@ -6,14 +6,23 @@ public class Chapter5 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        task2();
+        task3();
+    }
+
+    private static void task3() {
+        byte b = 5;
+        userForm("Patrik", "Göteborg", b);
+    }
+
+    private static void userForm(String name, String town, byte favoriteNum) {
+        System.out.println("Välkommen " + name + " från " + town + " med favoritnummer " + favoriteNum);
     }
 
     private static void task2() {
         System.out.printf("Add = %.2f\n", add(1.0, 1.2));
         System.out.println("Multi result: " + multi(2, 5, 10));
         System.out.println("Multi result: " + multi(new int[]{1, 2, 3, 4, 5, 6}));
-        System.out.println("Multi result: " + multi2(2,3,4,5,5,6,6,3,4,3,1));
+        System.out.println("Multi result: " + multi2(2, 3, 4, 5, 5, 6, 6, 3, 4, 3, 1));
     }
 
     private static double add(double x, double y) {
