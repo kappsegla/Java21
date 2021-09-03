4c9896e6440947db4516f20cb55de6122478496b
Martin Blomberg <martin.blomberg@outlook.com>
2021-09-03 09:13:15 +0200

    Adds exercise Chapter4 task1

---
+++ src/se/iths/java21/exercises/Chapter4.java
@@ -0,0 +1,27 @@
+package se.iths.java21.exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Chapter4 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        task2();
+    }
+
+    private static void task2() {
+
+
+    }
+
+    private static void task1() {
+        String[] colorArray = {"gul", "grön", "orange", "blå", "grå"};
+
+        for( String color : colorArray)
+            System.out.println(color);
+
+        //System.out.println(Arrays.toString(colorArray));
+        }
+    }
+
