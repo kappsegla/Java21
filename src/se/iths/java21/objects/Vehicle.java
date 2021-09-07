c4d09964856c31675f523f9495b88b882d7a0b59
Martin <martin.blomberg@outlook.com>
2021-09-07 10:04:04 +0200

    Added Vehicle class

---
+++ src/se/iths/java21/objects/Vehicle.java
@@ -0,0 +1,75 @@
+package se.iths.java21.objects;
+
+/**
+ * This class reprecents a Vehicle traveling on wheels.
+ */
+public class Vehicle {
+    private int passengers;
+    private int fuelCap;
+    private float kmPerLiter;
+    private boolean awd;
+
+    /**
+     *
+     * @param passengers
+     * @param fuelCap Current fuel capacity in litres.
+     * @param kmPerLiter
+     * @param awd
+     */
+    public Vehicle(int passengers, int fuelCap, float kmPerLiter, boolean awd) {
+        this.passengers = passengers;
+        this.fuelCap = fuelCap;
+        this.kmPerLiter = kmPerLiter;
+        this.awd = awd;
+    }
+
+    /**
+     * Returns the current maximum calculated distance this Vehicle can travel.
+     * @return Maximum distance in km.
+     */
+    public float computeRange(){
+        return fuelCap * kmPerLiter;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getFuelCap() {
+        return fuelCap;
+    }
+
+    public void setFuelCap(int fuelCap) {
+        this.fuelCap = fuelCap;
+    }
+
+    public float getKmPerLiter() {
+        return kmPerLiter;
+    }
+
+    public void setKmPerLiter(float kmPerLiter) {
+        this.kmPerLiter = kmPerLiter;
+    }
+
+    public boolean isAwd() {
+        return awd;
+    }
+
+    public void setAwd(boolean awd) {
+        this.awd = awd;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "passengers=" + passengers +
+                ", fuelCap=" + fuelCap +
+                ", kmPerLiter=" + kmPerLiter +
+                ", awd=" + awd +
+                '}';
+    }
+}
---
+++ src/se/iths/java21/objects/VehicleDemo.java
@@ -0,0 +1,15 @@
+package se.iths.java21.objects;
+
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle(4, 50, 14.5f, false);
+        Vehicle vehicle2 = new Vehicle(6, 70, 10.5f, false);
+        System.out.println(vehicle1);
+
+        System.out.println(vehicle1.computeRange());
+        System.out.println(vehicle2.computeRange());
+        System.out.printf("Computed range %.2f for this vehicle.%n",vehicle1.computeRange());
+
+
+    }
+}
