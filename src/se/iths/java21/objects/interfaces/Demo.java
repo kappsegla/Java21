7d5c3952077824903a27e500753377d934b1a072
Martin <martin.blomberg@outlook.com>
2021-09-13 15:18:19 +0200

    private constructor with of method

--- src/se/iths/java21/objects/interfaces/Demo.java
+++ src/se/iths/java21/objects/interfaces/Demo.java
@@ -1,14 +1,49 @@
 package se.iths.java21.objects.interfaces;
 
 
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
 
-        Pet myPet = new Cat();
+        Cat myCat = Cat.of("Flinga");
+        List<String> list = List.of("A","B","C");
+
+        Pet myPet = myCat;
+        Mammal myMammal = myCat;
+        Dog dog = new Dog();
+
+        System.out.println(myCat.talk());
+        System.out.println(myMammal.talk());
+        System.out.println(dog.talk());
     }
 }
 
-class Cat implements Pet, LandBound{
+abstract class Mammal {
+    private int weight;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    abstract String talk();
+}
+
+final class Cat extends Mammal implements Pet {
+
+    private String name;
+
+    private Cat(String name) {
+        this.name = name;
+    }
+
+    public static Cat of(String name){
+        return new Cat(name);
+    }
 
     @Override
     public boolean isCute() {
@@ -16,13 +51,32 @@ class Cat implements Pet, LandBound{
     }
 
     @Override
-    public float runningSpeed() {
-        return 25.0f;
+    String talk() {
+        return "Mjau";
     }
 }
 
+class Dog extends Mammal{
+
+    @Override
+    String talk() {
+        return "Voff";
+    }
+}
+
+
+@FunctionalInterface
 interface Pet {
-    boolean isCute();
+   boolean isCute();
+}
+
+interface FeedablePet extends Pet {
+    void feed();
+}
+
+
+interface MouseChase {
+    float runningSpeed();
 }
 
 interface LandBound {
