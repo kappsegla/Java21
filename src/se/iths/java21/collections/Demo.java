7cf4ab83a147fbb44e5f20728149f71bb6087bd2
Martin <martin.blomberg@outlook.com>
2021-09-20 14:24:54 +0200

    Collections

---
+++ src/se/iths/java21/collections/Demo.java
@@ -0,0 +1,46 @@
+package se.iths.java21.collections;
+
+import com.sun.source.doctree.SeeTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        //Add to the end O(n)
+        stringList.add("First");
+        stringList.add("Second");
+        //Find by index  O(1)
+        stringList.get(1);
+        //Insert O(n)
+        stringList.add(0,"Zero");
+        //Remove/Delete O(n)
+        stringList.remove(0);
+        //Search O(n)
+        stringList.indexOf("First");
+
+        List<Integer> integerList = new ArrayList<>();
+
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(6);
+        integerList.add(6);
+        integerList.add(5);
+        integerList.add(4);
+        integerList.add(4);
+        integerList.add(1);
+
+        integerList.forEach(System.out::println);
+
+        var set = Set.copyOf(integerList);
+
+        set.forEach(System.out::println);
+
+
+    }
+}
