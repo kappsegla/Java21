8a2c3a64e225ffcfd3fa40fc3be2c0b4e262679d
Martin Blomberg <martin.blomberg@outlook.com>
2021-09-24 14:05:50 +0200

    Adds exercise 7 Stack with Test methods

--- test/se/iths/java21/exercises/generics/MyStackTest.java
+++ test/se/iths/java21/exercises/generics/MyStackTest.java
@@ -1,6 +1,8 @@
 package se.iths.java21.exercises.generics;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,4 +35,39 @@ class MyStackTest {
         assertEquals("Second Item", myStack.pop());
         assertEquals("First Item", myStack.pop());
     }
+
+    @Test
+    void popBeforePushThrowsIllegalStateException() {
+        assertThrows(IllegalStateException.class, () -> myStack.pop() );
+    }
+
+    @Test
+    void popAfterTwoPushThreePop() {
+        myStack.push("First Item");
+        myStack.push("Second Item");
+
+        myStack.pop();
+        myStack.pop();
+        assertThrows(IllegalStateException.class, () -> myStack.pop() );
+    }
+
+    @Test
+    void peekReturnsLastPushedItemAndPopReturnsLastItem() {
+        myStack.push("First Item");
+        myStack.push("Second Item");
+
+        assertEquals("Second Item", myStack.peek());
+        assertEquals("Second Item", myStack.pop());
+    }
+
+    @Test
+    void sizeReturnsNumberOfItemsPushedToStack() {
+        myStack.push("One");
+        myStack.push("Two");
+        myStack.push("Three");
+        assertEquals(3, myStack.size());
+        myStack.pop();
+        assertEquals(2, myStack.size());
+    }
+
 }
