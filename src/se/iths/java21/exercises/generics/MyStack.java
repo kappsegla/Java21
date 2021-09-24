bc6e98cf3acf16952cccd4a241fa5622e35d12fb
Martin Blomberg <martin.blomberg@outlook.com>
2021-09-24 15:51:36 +0200

    Changes by Patrik

--- src/se/iths/java21/exercises/generics/MyStack.java
+++ src/se/iths/java21/exercises/generics/MyStack.java
@@ -3,13 +3,22 @@ package se.iths.java21.exercises.generics;
 public class MyStack<T> {
     private Node<T> head;
 
-    public void push(T obj){
+    public void push(T obj) {
+        Node<T> node = new Node<>();
+        node.data = obj;
 
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+        counter++;
     }
 
-    public T pop(){
-//        if (head == null)
-//            return null;
+    public T pop() {
+        if (head == null)
+            throw new IllegalStateException();
         var temp = head.data;
         head = head.next;
         return temp;
