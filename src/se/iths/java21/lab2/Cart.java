70c2b9fdf1b31d8148511e41725f772e2f5c3e34
Martin Blomberg <martin.blomberg@outlook.com>
2021-10-15 15:42:00 +0200

    Adds Cart with strategy for discounts.

--- src/se/iths/java21/lab2/Cart.java
+++ src/se/iths/java21/lab2/Cart.java
@@ -10,10 +10,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class Cart {
     private final List<Product> products = new ArrayList<>();
     private final List<Discount> discounts = new ArrayList<>();
+   // private final List<Function<List<Product>,BigDecimal>> discountList;
 
     public Cart() {
         discounts.add(new TenPercentIfOrderOver500());
