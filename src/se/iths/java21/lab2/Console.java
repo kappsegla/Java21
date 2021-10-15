2b3fa11fabb1cc7b088f618c18328c7d6fcd14a2
Martin Blomberg <martin.blomberg@outlook.com>
2021-10-15 09:28:47 +0200

    Adds Brand, Category and Product implementation

--- src/se/iths/java21/lab2/Console.java
+++ src/se/iths/java21/lab2/Console.java
@@ -2,8 +2,5 @@ package se.iths.java21.lab2;
 
 public class Console {
 
-    public static void printMobile(Mobile mobile){
-        System.out.println("Mobile :" + mobile.getName() + "," +mobile.getYear());
-    }
 
 }
--- src/se/iths/java21/lab2/entities/Product.java
+++ src/se/iths/java21/lab2/entities/Product.java
@@ -1,5 +1,66 @@
 package se.iths.java21.lab2.entities;
 
-public class Product {
 
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Product {
+    private final UUID id;
+    private final String name;
+    private final BigDecimal price;
+    private final Category categories;
+    private final Brand brand;
+
+    public Product(UUID id, String name, BigDecimal price, Category categories, Brand brand) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.categories = categories;
+        this.brand = brand;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Category getCategories() {
+        return categories;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(price, product.price) && Objects.equals(categories, product.categories) && Objects.equals(brand, product.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, categories, brand);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", categories=" + categories +
+                ", brand=" + brand +
+                '}';
+    }
 }
